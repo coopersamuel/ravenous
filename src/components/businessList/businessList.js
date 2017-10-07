44a86591a017f7e5495573b947372223cd0911f7
Samuel <samjcooper@eaton.com>
2017-10-06 21:07:25 -0400

    Setting up repo

---
+++ src/components/businessList/businessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './businessList.css';
+import Business from '../business/business.js';
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+                {this.props.business.map(business => {
+                    return <Business business={business}/>;
+                })}
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
